3cbe2979c73ed69a5c6964cb73fa7f00ae1e78ca
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2018-07-28 12:11:02 +0300

    add gulp tasks start and duild

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,46 @@
 'use strict';
 
 const gulp = require('gulp');
+const shell = require('gulp-shell');
 const protractor = require("gulp-protractor").protractor;
 
-gulp.task('default', () => {
+gulp.task('start', ['server'], () => {
     return gulp.src([])
-    .pipe(protractor({
-        configFile: "./configs/conf.js",
-        args: ['--baseUrl', 'http://127.0.0.1:8000']
-    }))
-    .on('error', function(e) { throw e })
+        .pipe(protractor({
+            configFile: "./configs/conf.js",
+            args: ['--baseUrl', 'http://127.0.0.1:8000']
+        }))
+        .on('error', (err) => {
+            throw err;
+        });
 });
+
+gulp.task('server', ['linter'], (done) => {
+    gulp.src('*.js', { read: false })
+        .pipe(shell([
+            'start cmd /k "node_modules\\.bin\\webdriver-manager start"'
+        ]));
+    setTimeout(() => {
+        done();
+    }, 3000);
+});
+
+
+gulp.task('npm_install', () => {
+    return gulp.src('*.js', { read: false })
+        .pipe(shell([
+            'npm install'
+        ]));
+});
+
+gulp.task('linter', () => {
+    return gulp.src('*.js', { read: false })
+        .pipe(shell([
+            'eslint ./ --fix'
+        ]));
+});
+
+gulp.task('build', ['npm_install', 'linter'], () => {
+
+});
+gulp.task('default', ['start']);
